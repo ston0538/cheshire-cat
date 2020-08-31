dcad1ca55e5bd8ccdf36490e66de3b501b6363e5
KHK <ston0538@gmail.com>
2020-09-01 02:49:29 +0900

    Initial Commit

---
+++ src/cheshire_cat/handler.clj
@@ -0,0 +1,15 @@
+(ns cheshire-cat.handler
+  (:require [compojure.core :refer :all]
+            [compojure.route :as route]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [cheshire.core :as json]))
+
+(defroutes app-routes
+  (GET "/" [] "Hello World")
+  (GET "/cheshire-cat" [] {:status 200 :headers {"Content-Type" "application/json; charset=utf-8"}
+                           :body (json/generate-string
+                                  {:name "KHK" :status :grinning})})
+  (route/not-found "Not Found"))
+
+(def app
+  (wrap-defaults app-routes site-defaults))
